f383bc8bd2d9fbe664704aff6c9b3e3715855df8
Espenvb <espenvb12@gmail.com>
2024-01-10 10:48:22 +0100

    Oppgave 4 Go

--- Ex1/shared variable/go/foo.go
+++ Ex1/shared variable/go/foo.go
@@ -10,28 +10,54 @@ import (
 
 var i = 0
 
-func incrementing() {
-    //TODO: increment i 1000000 times
-    for a := 0; a < 1000000; a++{
-        i++;
+func server(increment chan int, quitInc chan int, decrement chan int, quitDec chan int){
+    for {
+        select{
+        case <- increment:
+            i++
+        case <- decrement:
+            i--
+        case <- quitDec:
+            return
+        }
     }
+
 }
 
-func decrementing() {
-    //TODO: decrement i 1000000 times
-    for a := 0; a < 1000000; a++{
-        i--;
+func incrementing(increment chan int, quitInc chan int) {
+    for a := 0; a < 1000; a++{
+        increment <- 1
     }
+    quitInc <- 1
 }
+
+func decrementing(decrement chan int, quitDec chan int) {
+    for a := 0; a < 10; a++{
+        decrement <- 1
+    }
+    quitDec <- 1
+}
+
 
 func main() {
     // What does GOMAXPROCS do? What happens if you set it to 1?
     runtime.GOMAXPROCS(2)    
 	
-    // TODO: Spawn both functions as goroutines
+    increment := make(chan int)
+    decrement := make(chan int)
+    quitInc := make(chan int)
+    quitDec := make(chan int)
 
-    go increment()
-    go decrement()
+
+
+    // TODO: Spawn both functions as goroutines
+    go incrementing(increment, quitInc)
+    go decrementing(decrement, quitDec)
+    go server(increment, quitInc, decrement, quitDec)
+
+
+
+    
 	
     // We have no direct way to wait for the completion of a goroutine (without additional synchronization of some sort)
     // We will do it properly with channels soon. For now: Sleep.
