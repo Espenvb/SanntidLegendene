0b14e9305dcd4abcdb99e4ac7a7f46789b677ec8
name <email@example.com>
2024-01-17 12:14:56 +0100

    New Lars stuff

--- Ex2/main2.go
+++ Ex2/main2.go
@@ -1,79 +1,75 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
-	"fmt"
 )
 
-
-
-
-
-
-func main(){
+func getIP() {
 	port := 30000
-	portsend := 20010
 
-
-	
-
-	addr, err := net.ResolveUDPAddr("udp",fmt.Sprintf(":%d",port))
-	if err!=nil {
+	addr, err := net.ResolveUDPAddr("udp", fmt.Sprintf(":%d", port))
+	if err != nil {
 		log.Fatal(err)
 	}
 
-	conn, err := net.ListenUDP("udp",addr)
-	if err!=nil {
+	conn, err := net.ListenUDP("udp", addr)
+	if err != nil {
 		log.Fatal(err)
 	}
 
 	defer conn.Close()
 
-	
-	buffer := make([]byte,1024)
+	buffer := make([]byte, 1024)
 
 	n, _, err := conn.ReadFromUDP(buffer)
-	if err!=nil {t
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	message := string(buffer[:n])
 	fmt.Printf("melding:", message)
+}
 
-
-
-	addr2, err := net.ResolveUDPAddr("udp",fmt.Sprintf(":%d",portsend))
-
-	udpaddr := &net.UDPAddr{
-		IP:	net.ParseIP("10.100.23.129"),
-		Port: 20010,
-	}
-	
+func sendmessage(halla *net.UDPConn, udpaddr *net.UDPAddr) {
 	svar := []byte("heihei")
-
-
-
-
-	høre, err := net.ListenUDP("udp", addr2)
-
-
-	_, err = conn.WriteToUDP(svar,udpaddr)
+	_, err := halla.WriteToUDP(svar, udpaddr)
 	if err != nil {
 		log.Fatal(err)
 	}
-	nybuffer := make([]byte,1024)
-	p, _, err := høre.ReadFromUDP(nybuffer)
-	if err!=nil {
+
+}
+
+func readmessage(halla *net.UDPConn, udpaddr *net.UDPAddr) {
+	nybuffer := make([]byte, 1024)
+	p, _, err := halla.ReadFromUDP(nybuffer)
+	if err != nil {
 		log.Fatal(err)
 	}
 
+	fmt.Printf("svar:", string(nybuffer[:p]))
 
-	fmt.Printf("svar:",string(nybuffer[:p]))
-
-
-
-
-	
-	
 }
-	
+
+func main() {
+	serverPort := 20010
+
+	udpaddr := &net.UDPAddr{
+		IP:   net.ParseIP("10.100.23.129"),
+		Port: 20010,
+	}
+
+	addr2, err := net.ResolveUDPAddr("udp", fmt.Sprintf(":%d", serverPort))
+
+	høre, err := net.ListenUDP("udp", addr2)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	getIP()
+	sendmessage(høre, udpaddr)
+	readmessage(høre, udpaddr)
+
+}
