28ef3c856cda04abad9a17b503511260fb49b68f
name <email@example.com>
2024-01-17 11:44:35 +0100

    fikk til udp stygt

--- Ex2/main2.go
+++ Ex2/main2.go
@@ -6,8 +6,17 @@ import (
 	"fmt"
 )
 
+
+
+
+
+
 func main(){
 	port := 30000
+	portsend := 20010
+
+
+	
 
 	addr, err := net.ResolveUDPAddr("udp",fmt.Sprintf(":%d",port))
 	if err!=nil {
@@ -25,13 +34,45 @@ func main(){
 	buffer := make([]byte,1024)
 
 	n, _, err := conn.ReadFromUDP(buffer)
-	if err!=nil {
-		log.Fatal(err)
-		return
+	if err!=nil {t
 	}
 
 	message := string(buffer[:n])
 	fmt.Printf("melding:", message)
+
+
+
+	addr2, err := net.ResolveUDPAddr("udp",fmt.Sprintf(":%d",portsend))
+
+	udpaddr := &net.UDPAddr{
+		IP:	net.ParseIP("10.100.23.129"),
+		Port: 20010,
+	}
+	
+	svar := []byte("heihei")
+
+
+
+
+	høre, err := net.ListenUDP("udp", addr2)
+
+
+	_, err = conn.WriteToUDP(svar,udpaddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	nybuffer := make([]byte,1024)
+	p, _, err := høre.ReadFromUDP(nybuffer)
+	if err!=nil {
+		log.Fatal(err)
+	}
+
+
+	fmt.Printf("svar:",string(nybuffer[:p]))
+
+
+
+
 	
 	
 }
